fe094b7798c1b691814b5d8fb6b34695c45eea68
odiilbekov <alibekodilbekov@gmail.com>
2025-11-11 14:37:51 +0500

    massiv dagi malumotni htmlda ishla dik

---
+++ table.php
@@ -0,0 +1,34 @@
+<?php 
+$talabalar = [
+[ 'ism' =>"shamshod", 'familya'=>"abdullayev", 'yosh'=>16, 'baxosi'=> 5 ],
+[ 'ism' =>"shamshod", 'familya'=>"abdullayev", 'yosh'=>16, 'baxosi'=> 5 ],
+[ 'ism' =>"shamshod", 'familya'=>"abdullayev", 'yosh'=>16, 'baxosi'=> 5 ],
+[ 'ism' =>"shamshod", 'familya'=>"abdullayev", 'yosh'=>16, 'baxosi'=> 5 ],
+[ 'ism' =>"shamshod", 'familya'=>"abdullayev", 'yosh'=>16, 'baxosi'=> 5 ],
+[ 'ism' =>"shamshod", 'familya'=>"abdullayev", 'yosh'=>16, 'baxosi'=> 5 ],
+[ 'ism' =>"shamshod", 'familya'=>"abdullayev", 'yosh'=>16, 'baxosi'=> 5 ],
+]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+       <thead>
+            <tr>
+                <th>#</th>
+                <th>ism</th>
+                <th>familya</th> 
+                <th>yosh</th> 
+                <th>bahosi</th> 
+            </tr>
+     </thead>
+  <tbody>
+  </tbody>
+</table>
+</body>
+</html>
