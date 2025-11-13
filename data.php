3aadb84f671886c11bbddfadce2336f3550423f3
odiilbekov <alibekodilbekov@gmail.com>
2025-11-13 16:27:19 +0500

    time_date

--- data.php
+++ data.php
@@ -1,7 +1,13 @@
 <?php 
 // var_dump($_POST);
 // var_dump($_GET)
-echo $_POST['sarlavha'];
+echo $_POST['sarlavha']."<br>";
+echo $_POST['matn']. "<br>";
+// $_REQUEST;
+var_dump($_REQUEST);
+echo $_REQUEST['sarlavha'];
+
+
 
 
 ?>
---
+++ timedate.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set("asia/tashkent");
+echo date("y.m.d")."<br>";
+echo date("y.m.d H:i:s")."<br>";
+echo date("D F Y")."<br>";
+echo date('d.m.y',  strtotime( "2025-11-11"))
+
+
+
+?>
